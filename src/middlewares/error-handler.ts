cb31cee01663b8e565331644f3ea4affed84f84a
Niranjan Avanippilli Santhosh <niranjan.santhosh@agilepoint.com>
2024-03-31 13:17:47 +0530

    chore: error handler middleware and custom error class

---
+++ src/middlewares/error-handler.ts
@@ -0,0 +1,19 @@
+import { ErrorRequestHandler } from "express";
+import { APIError } from "../lib/api-error";
+
+export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+    let error = err;
+
+    if (!(err instanceof APIError)) {
+        const statusCode = err.statusCode || 500;
+        const message = err.message || 'Something went wrong!';
+        error = new APIError(statusCode, message, err?.errors || [], err?.stack);
+    }
+    const response = {
+        ...error,
+        message: error?.message,
+        ...(process.env.NODE_ENV === "development" ? { stack: error.stack } : {})
+    }
+
+    return res.status(error.statusCode).json(response);
+}
