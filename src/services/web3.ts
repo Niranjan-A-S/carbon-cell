4b0087230de2145c8a8c6ad2ff9db859d5b3fa5c
Niranjan Avanippilli Santhosh <niranjan.santhosh@agilepoint.com>
2024-04-01 15:26:07 +0530

    Revert "chore: trial declaration"
    This reverts commit 5daf0ed30831eca26c38c68940d7ab4e6c457407.

--- src/services/web3.ts
+++ src/services/web3.ts
@@ -1,9 +1,3 @@
 import Web3 from "web3";
 
-declare global {
-    namespace web3 {
-        const web3Service: Web3
-    }
-}
-
 export const web3Service = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`));
